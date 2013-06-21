b043d1a141c41a29cb1645a456fd1e513a988a1b
Никита Володин [Nikita Volodin] <volodin.n@gmail.com>
2013-06-21 20:21:04 -0300

    (computer): add initial code for touchscreen of computer_manager

---
+++ computer_manager.lua
@@ -0,0 +1,25 @@
+os.unloadAPI("buttons")
+os.loadAPI("buttons")
+
+local running = true
+local allButtons = {}
+
+monitor = peripheral.wrap("right")
+
+exit = function()
+	running = false
+end
+
+allButtons.exit = buttons.register(10, 10, 10, 3, colors.white, colors.grey, "exit", exit)
+
+
+buttons.draw()
+while running do 
+	local eventArray = {os.pullEvent()}
+	buttons.event(eventArray)
+	buttons.draw()
+end
+
+monitor.clear()
+
+os.unloadAPI("buttons")
