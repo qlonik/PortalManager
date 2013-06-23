b7af611a8b7631218a63ffc64090af12459e4313
Никита Володин [Nikita Volodin] <volodin.n@gmail.com>
2013-06-22 22:19:59 -0300

    feat (turtle): add program for turtle portal book manager
    Add method to check in to computer screen manager
    Add methods to open and close portal with given slot number from chest

---
+++ turtle_manager.lua
@@ -0,0 +1,46 @@
+--	Requirements for this project:
+--		Wireless inventory turtle
+
+local modemSide = "right"
+local inv = peripheral.wrap("left")
+
+local screenManager
+
+local running = true
+
+findScreenManager = function()
+	while message ~= "portal_screen_manager" do
+		local senderID, message, distance = rednet.receive()
+		screenManager = senderID
+	end
+
+	rednet.send(screenManager, "portal_book_manager")
+end
+
+openPortal = function(slot)
+	turtle.select(1)
+	inv.suck(slot, 1)
+	turtle.dropUp()
+end
+
+closePortal = function(slot)
+	turtle.select(1)
+	turtle.suckUp()
+	inv.drop(slot, 1)
+end
+
+rednet.open(modemSide)
+findScreenManager()
+
+while running do
+	local senderID, message, distance = rednet.receive()
+	command = textutils.unserialize(message)
+
+	if command["command"] == "open" then
+		openPortal(command["slot"])
+	elseif command["command"] == "close" then
+		closePortal(command["slot"])
+	end
+end
+
+rednet.close(modemSide)
